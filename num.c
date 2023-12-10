c173cc896069b8eb65013c8e644de3476682a2b6
Ranvir11Master <master11ranvir@gmail.com>
2023-12-10 16:16:19 +0530

    addchane

---
+++ num.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// Checking even or odd
+//even -> 1
+//odd ->0
+int main() {
+    int x;
+    printf("enter a num : ");
+    scanf("%d",&x);
+
+    printf("%d\n",x % 2 == 0);
+
+}
